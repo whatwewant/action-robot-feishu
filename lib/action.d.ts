98c3d67e0001681e8bcee4d34f972a047991f2df
Zero <tobewhatwewant@gmail.com>
2021-05-16 00:25:10 +0800

    chore: remove lib

--- lib/action.d.ts
+++
@@ -1,3 +0,0 @@
-import { IActionInput } from './type';
-export declare function getActionInput(): IActionInput;
-export declare function sendAction(): Promise<void>;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IActionInput, IMessage } from './type';
-export declare function createMessage(actionInput: IActionInput): IMessage;
-export declare function post(url: string, body: IMessage): Promise<string>;
-export declare function sendMessage(actionInput: IActionInput): Promise<string>;
