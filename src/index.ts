b77278d23b4a6b4b75656d7983c6d3fb07df7b82
whatwewant <tobewhatwewant@outlook.com>
2021-05-23 19:01:58 +0800

    chore: use @zodash/nobot sdk instead of raw request

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,10 @@
-import doreamon from '@zodash/doreamon';
+import notify from '@zodash/nobot';
 
-import { IActionInput, IMessage } from './type';
+import { IActionInput } from './type';
 
-export function createMessage(actionInput: IActionInput): IMessage {
-  const data: any[] = [
-    {
-      tag: 'text',
-      text: actionInput.text,
-    },
-  ];
-
-  if (actionInput.at) {
-    data.push({
-      tag: 'at',
-      user_id: actionInput.at,
-    });
-  }
-
-  return {
-    msg_type: 'post',
-    content: {
-      post: {
-        zh_cn: {
-          title: actionInput.title,
-          content: [data],
-        },
-      },
-    },
-  };
-}
-
-export async function post(url: string, body: IMessage): Promise<string> {
-  const response = await doreamon.request
-    .post(url, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body,
-    })
-    .json();
-
-  return response;
-}
-
-export async function sendMessage(actionInput: IActionInput): Promise<string> {
-  const message = createMessage(actionInput);
-  return post(actionInput.url, message);
+export async function sendMessage(actionInput: IActionInput): Promise<void> {
+  return notify('feishu', actionInput.url, {
+    title: actionInput.title,
+    content: actionInput.text,
+  });
 }
--- src/type.ts
+++ src/type.ts
@@ -4,12 +4,3 @@ export interface IActionInput {
   text: string;
   at?: string;
 }
-
-export interface IMessage {
-  msg_type: 'text' | 'post';
-  content: IContent;
-}
-
-export interface IContent {
-  // @TODO
-}
